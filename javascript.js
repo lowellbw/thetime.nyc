3a32a8420bbedffa61aed39869e903fc37ff62a2
Lowell Weisbord <lowellweisbord@gmail.com>
2022-03-05 20:07:20 +0000

    lmost working

--- javascript.js
+++ javascript.js
@@ -1,7 +1,22 @@
+moment().format();
 
-function theTimeInNYC(){
-    document.querySelector('h2').textContent = Date.now();
+
+function updateTime() {
+    let time = moment().tz("America/New_york").format("h:mm:ss a");
+    document.querySelector('h2').textContent = time;
+    setInterval(updateTime, 1000);
+    updateTime;
+}
+function initializeTime(){
+    const clock = document.createElement('h2');
+    const element = document.getElementById("the-time");
+    element.appendChild(clock);
+    updateTime();
+
 }
 
-var bool = moment().tz("America-new_york")
-console.log(bool);
+function reformat(){
+    button = document.querySelector('button');
+    button.classList.toggle('fade');
+    initializeTime();
+}
