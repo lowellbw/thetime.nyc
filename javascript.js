20ef4067c27bbb87ecd696a963318af6c0dddd88
Lowell Weisbord <lowellweisbord@gmail.com>
2022-03-12 21:39:44 +0000

    added all poster images

--- javascript.js
+++ javascript.js
@@ -10,24 +10,25 @@ function updateTime() {
 }
 document.onload = updateTime();
 
-function updateVideoBackground(new_source) {
+function updateVideoBackground(new_source, new_poster) {
     let background = document.querySelector('video');
     background.src = new_source;
+    background.poster = new_poster
 }
 home = document.querySelector('h1');
-home.addEventListener("click", function(){updateVideoBackground('background-shortened.mp4')});
+home.addEventListener("click", function(){updateVideoBackground('background-shortened.mp4', 'background-m.jpeg')});
 
 brooklyn = document.getElementById('brooklyn');
-brooklyn.addEventListener("click", function(){updateVideoBackground('brooklyn.mp4')});
+brooklyn.addEventListener("click", function(){updateVideoBackground('brooklyn.mp4', 'background-b.jpeg')});
 
 manhattan = document.getElementById('manhattan');
-manhattan.addEventListener("click", function(){updateVideoBackground('manhattan.mp4')});
+manhattan.addEventListener("click", function(){updateVideoBackground('manhattan.mp4', 'background-manhattan.jpeg')});
 
 bronx = document.getElementById('bronx');
-bronx.addEventListener("click", function(){updateVideoBackground('bronx.mp4')});
+bronx.addEventListener("click", function(){updateVideoBackground('bronx.mp4', 'background-bronx.jpeg')});
 
 queens = document.getElementById('queens');
-queens.addEventListener("click", function(){updateVideoBackground('queens.mp4')});
+queens.addEventListener("click", function(){updateVideoBackground('queens.mp4', 'background-q.jpeg')});
 
 staten = document.getElementById('staten');
-staten.addEventListener("click", function(){updateVideoBackground('statenisland.mp4')});
+staten.addEventListener("click", function(){updateVideoBackground('statenisland.mp4', 'background-staten.jpeg')});
